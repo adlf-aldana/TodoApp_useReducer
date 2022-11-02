b16a471e333f343d7c88dd7ad532cd60b6942698
Adolfo <adlf.aldana@gmail.com>
2022-11-01 23:56:16 -0400

    #feat: useReducer - Todo List

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { TodoApp } from "./TodoApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Inicializando proyecto TODO</h1>
+    <TodoApp />
   </React.StrictMode>
 );
