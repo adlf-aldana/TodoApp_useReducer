687240cd2554518c95034151e8c06f2a0ab1541c
Adolfo <adlf.aldana@gmail.com>
2022-11-02 16:36:54 -0400

    #feat: hook useForm, components TodoAdd, TodoItem, TodoList and emission of events

---
+++ src/TodoAdd.jsx
@@ -0,0 +1,37 @@
+import { useForm } from "./hooks/useForm";
+
+export const TodoAdd = ({ handleNewTodo }) => {
+  const { description, onInputChange, onResetForm } = useForm({
+    description: "",
+  });
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (description.length <= 1) return;
+
+    const newTodo = {
+      id: new Date().getTime(),
+      description,
+      done: false,
+    };
+
+    handleNewTodo(newTodo);
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="¿Que hay que hacer?"
+        className="form-control"
+        name="description"
+        value={description}
+        onChange={onInputChange}
+      />
+
+      <button type="submit" className="btn btn-outline-primary mt-1">
+        Agregar
+      </button>
+    </form>
+  );
+};
--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,4 +1,6 @@
 import { useReducer } from "react";
+import { TodoAdd } from "./TodoAdd";
+import { TodoList } from "./TodoList";
 import { todoReducer } from "./todoReducer";
 
 const initialState = [
@@ -9,13 +11,17 @@ const initialState = [
   },
   {
     id: new Date().getTime() * 3,
-    description: "Recolectar la piedra del alma",
+    description: "Recolectar la piedra del tiempo",
     done: false,
   },
 ];
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+  const handleNewTodo = (newTodo) => {
+    console.log(newTodo);
+  };
 
   return (
     <>
@@ -26,33 +32,13 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <ul className="list-group">
-            {todos.map((todo) => (
-              <li
-                key={todo.id}
-                className="list-group-item d-flex justify-content-between"
-              >
-                <span className="align-self-center">Item 1</span>{" "}
-                <button className="btn btn-danger">Borrar</button>
-              </li>
-            ))}
-          </ul>
+          <TodoList todos={todos} />
         </div>
 
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-          <form>
-            <input
-              type="text"
-              placeholder="¿Que hay que hacer?"
-              className="form-control"
-            />
-
-            <button type="submit" className="btn btn-outline-primary mt-1">
-              Agregar
-            </button>
-          </form>
+          <TodoAdd handleNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
