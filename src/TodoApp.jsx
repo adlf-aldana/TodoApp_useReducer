11b32f612024775e5a6b16a248a6b24e2b308fc9
Adolfo <adlf.aldana@gmail.com>
2022-11-02 21:39:01 -0400

    #feat: delete  a TODO

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -25,6 +25,14 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleDeleteTodo = (id) => {
+    console.log(id);
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
   return (
     <>
       <h1>
@@ -34,7 +42,7 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
         </div>
 
         <div className="col-5">
