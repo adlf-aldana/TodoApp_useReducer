3335e7f4bf7bcc48ce498bafbad8c31e66dc2a04
Adolfo <adlf.aldana@gmail.com>
2022-11-02 22:28:59 -0400

    #perf: hook - useTodos

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,49 +1,21 @@
-import { useEffect, useReducer } from "react";
+import { useTodos } from "./hooks/useTodos";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer";
-
-const initialState = [];
-
-const init = () => {
-  return JSON.parse(localStorage.getItem("todos")) || [];
-};
 
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  const handleNewTodo = (todo) => {
-    const action = {
-      type: "[TODO] Add Todo",
-      payload: todo,
-    };
-
-    dispatch(action);
-  };
-
-  const handleDeleteTodo = (id) => {
-    console.log(id);
-    dispatch({
-      type: "[TODO] Remove Todo",
-      payload: id,
-    });
-  };
-
-  const handleToggleTodo = (id) => {
-    dispatch({
-      type: "[TODO] Toggle Todo",
-      payload: id,
-    });
-  };
+  const {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  } = useTodos();
 
   return (
     <>
       <h1>
-        TodoApp: 10, <small>Pendientes: 2</small>
+        TodoApp: {todosCount},<small>Pendientes: {pendingTodosCount}</small>
       </h1>
       <hr />
 
